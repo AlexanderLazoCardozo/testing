6331360bd1d855669b4cee69ff1c6b79a50f4f07
Alexander Lazo <88220347+AlexanderLazoCardozo@users.noreply.github.com>
2022-03-30 22:01:07 -0500

    FirstUpload

---
+++ assets/index.a8c404e8.js
@@ -0,0 +1,106 @@
+import{j as i,F as c,a as e,S as C,E as N,P as R,b as s,C as y,I as r,B as l,s as x,r as T,c as k,d as M,e as O,f as L,N as b,A as F,g as B,u as z,R as w,G as f,h as o,i as p,V as m,M as S,H as P,k as D,l as G,m as g,n as W}from"./vendor.a7c2ee93.js";const j=function(){const h=document.createElement("link").relList;if(h&&h.supports&&h.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))u(t);new MutationObserver(t=>{for(const d of t)if(d.type==="childList")for(const I of d.addedNodes)I.tagName==="LINK"&&I.rel==="modulepreload"&&u(I)}).observe(document,{childList:!0,subtree:!0});function a(t){const d={};return t.integrity&&(d.integrity=t.integrity),t.referrerpolicy&&(d.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?d.credentials="include":t.crossorigin==="anonymous"?d.credentials="omit":d.credentials="same-origin",d}function u(t){if(t.ep)return;t.ep=!0;const d=a(t);fetch(t.href,d)}};j();function E(){return i(c,{children:[i("div",{style:{textAlign:"center"},children:[e("div",{style:{color:"#00267d",marginTop:"40px"},children:e("h1",{style:{fontSize:"60px"},children:"\xA1TE OFRECEMOS EL INTERNET"})}),e("br",{}),e("div",{style:{color:"#66277d"},children:e("h3",{style:{fontSize:"30px"},children:"M\xC1S ESTABLE A PRECIOS DE TU ALCANCE!"})})]}),i(C,{effect:"coverflow",grabCursor:!0,centeredSlides:!0,slidesPerView:"auto",coverflowEffect:{rotate:50,stretch:0,depth:100,modifier:1,slideShadows:!1},pagination:!0,modules:[N,R],className:"mySwiper",children:[e(s,{children:i(y,{id:"card",style:{borderRadius:"15px",width:"85%",boxShadow:"0px 0px 20px #66277d",textAlign:"center",marginLeft:"25px"},children:[e("br",{})," ",e("br",{}),e("div",{children:e("b",{style:{fontSize:"45px",color:"#00267d"},children:"30 MB"})}),e("div",{children:e("b",{style:{color:"#00267d"},children:"INTERNET DE FIBRA OPTICA"})}),e("br",{}),i("div",{children:[e("b",{style:{fontSize:"25px",marginBottom:"1000px",color:"#66277d"},children:" S/.   "}),e("b",{style:{fontSize:"60px",color:"#66277d"},children:"75 "})]}),e("br",{}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"RAUTER WIFI"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"INTERNET SIM\xC9TRICO"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"DECARGAS Y CARGAS ILIMITADAS"})]}),e("br",{}),e("div",{children:i(l,{style:{color:"white",backgroundColor:"#66277d",width:"50%"},animated:"fade",children:[e(l.Content,{visible:!0,children:"SOLICITAR"}),e(l.Content,{hidden:!0,children:e(r,{name:"shop"})})]})})]})}),e(s,{children:i(y,{id:"card",style:{borderRadius:"15px",width:"85%",boxShadow:"0px 0px 20px #66277d",textAlign:"center",marginLeft:"25px"},children:[e("br",{})," ",e("br",{}),e("div",{children:e("b",{style:{fontSize:"45px",color:"#00267d"},children:"50 MB"})}),e("div",{children:e("b",{style:{color:"#00267d"},children:"INTERNET DE FIBRA OPTICA"})}),e("br",{}),i("div",{children:[e("b",{style:{fontSize:"25px",marginBottom:"1000px",color:"#66277d"},children:" S/.   "}),e("b",{style:{fontSize:"60px",color:"#66277d"},children:"85 "})]}),e("br",{}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"RAUTER WIFI"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"INTERNET SIM\xC9TRICO"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"DECARGAS Y CARGAS ILIMITADAS"})]}),e("br",{}),e("div",{children:i(l,{style:{color:"white",backgroundColor:"#66277d",width:"50%"},animated:"fade",children:[e(l.Content,{visible:!0,children:"SOLICITAR"}),e(l.Content,{hidden:!0,children:e(r,{name:"shop"})})]})})]})}),e(s,{children:i(y,{id:"card",style:{borderRadius:"15px",width:"85%",boxShadow:"0px 0px 20px #66277d",textAlign:"center",marginLeft:"25px"},children:[e("br",{})," ",e("br",{}),e("div",{children:e("b",{style:{fontSize:"45px",color:"#00267d"},children:"80 MB"})}),e("div",{children:e("b",{style:{color:"#00267d"},children:"INTERNET DE FIBRA OPTICA"})}),e("br",{}),i("div",{children:[e("b",{style:{fontSize:"25px",marginBottom:"1000px",color:"#66277d"},children:" S/.   "}),e("b",{style:{fontSize:"60px",color:"#66277d"},children:"99 "})]}),e("br",{}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"RAUTER WIFI"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"INTERNET SIM\xC9TRICO"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"DECARGAS Y CARGAS ILIMITADAS"})]}),e("br",{}),e("div",{children:i(l,{style:{color:"white",backgroundColor:"#66277d",width:"50%"},animated:"fade",children:[e(l.Content,{visible:!0,children:"SOLICITAR"}),e(l.Content,{hidden:!0,children:e(r,{name:"shop"})})]})})]})}),e(s,{children:i(y,{id:"card",style:{borderRadius:"15px",width:"85%",boxShadow:"0px 0px 20px #66277d",textAlign:"center",marginLeft:"25px"},children:[e("br",{})," ",e("br",{}),e("div",{children:e("b",{style:{fontSize:"45px",color:"#00267d"},children:"100 MB"})}),e("div",{children:e("b",{style:{color:"#00267d"},children:"INTERNET DE FIBRA OPTICA"})}),e("br",{}),i("div",{children:[e("b",{style:{fontSize:"25px",marginBottom:"1000px",color:"#66277d"},children:" S/.   "}),e("b",{style:{fontSize:"60px",color:"#66277d"},children:"119 "})]}),e("br",{}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"RAUTER WIFI"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"INTERNET SIM\xC9TRICO"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"DECARGAS Y CARGAS ILIMITADAS"})]}),e("br",{}),e("div",{children:i(l,{style:{color:"white",backgroundColor:"#66277d",width:"50%"},animated:"fade",children:[e(l.Content,{visible:!0,children:"SOLICITAR"}),e(l.Content,{hidden:!0,children:e(r,{name:"shop"})})]})})]})}),e(s,{children:i(y,{id:"card",style:{borderRadius:"15px",width:"85%",boxShadow:"0px 0px 20px #66277d",textAlign:"center",marginLeft:"25px"},children:[e("br",{})," ",e("br",{}),e("div",{children:e("b",{style:{fontSize:"45px",color:"#00267d"},children:"150 MB"})}),e("div",{children:e("b",{style:{color:"#00267d"},children:"INTERNET DE FIBRA OPTICA"})}),e("br",{}),i("div",{children:[e("b",{style:{fontSize:"25px",marginBottom:"1000px",color:"#66277d"},children:" S/.   "}),e("b",{style:{fontSize:"60px",color:"#66277d"},children:"159 "})]}),e("br",{}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"RAUTER WIFI"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"INTERNET SIM\xC9TRICO"})]}),i("div",{style:{textAlign:"left",margin:"10px 20px"},children:[e(r,{name:"check circle outline"}),e("b",{style:{display:"inline"},children:"DECARGAS Y CARGAS ILIMITADAS"})]}),e("br",{}),e("div",{children:i(l,{style:{color:"white",backgroundColor:"#66277d",width:"50%"},animated:"fade",children:[e(l.Content,{visible:!0,children:"SOLICITAR"}),e(l.Content,{hidden:!0,children:e(r,{name:"shop"})})]})})]})})]}),e("br",{}),e("div",{style:{color:"#00267d",textAlign:"center"},children:e("b",{children:"(*)Pago \xFAnico de instalacion del servicio S/150.00 - Precios Incluyen IGV"})})]})}const V=x.div`
+  width: 100%;
+  height: 70px;
+  background-color: white;
+`,U=x.div`
+  width: 100%;
+  max-width: 1400px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+`,Y=x.div`
+  margin-left: 0.5rem;
+  font-size: 1.5rem;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+`,q=x.ul`
+margin:0;
+  z-index: 999;
+paddingLeft: 100px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  @media screen and (max-width: 960px) {
+    position: absolute;
+    top: 70px;
+    left: ${({showMobileMenu:n})=>n?"0":"-100%"};
+    width: 100%;
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+    transition: 0.8s all ease;
+  }
+`,A=x.li`
+  margin:0;
+  z-index: 999;
+  cursor: pointer;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: flex-end;
+  background-color: white;
+  transition: 0.3s all ease;
+  hr {
+    text-align: center;
+    width: 0;
+    border: none;
+    border-bottom: 0.3rem solid #5b287a;
+    transition: 0.3s all ease;
+  }
+  &:hover {
+    background-color: white;
+    transition: 0.8s all ease;
+    hr {
+      width: 100%;
+      transition: 0.8s all ease;
+      box-shadow: 1px -10px 20px  #B886D9;
+    }
+  }
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    height: 70px;
+  }
+`,v=x.a`
+  padding: 0.5rem 4rem;
+  color: #5b287a;
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+  width: 100%;
+  text-decoration: none;
+`,_=x.div`
+  display: none;
+  @media screen and (max-width: 960px) {
+    display: flex;
+    margin-right: 0.5rem;
+    svg {
+      animation: ${({showMobileMenu:n})=>n?"rotationMoveInitial":"rotationMoveReverse"};
+      animation-duration: 0.3s;
+    }
+    @keyframes rotationMoveInitial {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(90deg);
+      }
+    }
+    @keyframes rotationMoveReverse {
+      0% {
+        transform: rotate(90deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
+  }
+`;var H="/assets/wasiLogo.a0a06ac2.png";const $=()=>{const[n,h]=T.exports.useState(!1),a=()=>{h(!n)};return e(c,{children:e(k,{children:e(V,{children:e(U,{children:i(M.Provider,{value:{color:"#5b287a",size:"1.8em"},children:[e(Y,{showMobileMenu:n,children:e("img",{style:{width:"100px"},src:H})}),e(_,{onClick:()=>a(),children:n?e(O,{}):e(L,{})}),i(q,{showMobileMenu:n,children:[e(b,{to:"/",children:i(A,{onClick:()=>a(),children:[e(v,{children:"INICIO"}),e("hr",{})]})}),e(b,{to:"/Nosotros",children:i(A,{onClick:()=>a(),children:[e(v,{children:"NOSOTROS"}),e("hr",{})]})}),e(b,{to:"/Contacto",children:i(A,{onClick:()=>a(),children:[e(v,{children:"CONTACTO"}),e("hr",{})]})}),e(b,{to:"/Planes",children:i(A,{onClick:()=>a(),children:[e(v,{children:" PLANES "}),e("hr",{})]})}),e(b,{to:"/Solicitar",children:i(A,{onClick:()=>a(),children:[e(v,{style:{fontSize:"20px"},children:" SOLICITAR "}),e("hr",{})]})})]})]})})})})})};var K="/assets/BANNER1.5c7e07f8.png",J="/assets/BANNER2.cc547c07.png",Q="/assets/BANNER3.eaec3e16.png";function X(){return e(c,{children:i(C,{style:{padding:"0px"},autoHeight:!0,pagination:{type:"progressbar"},navigation:!0,modules:[F,R,B],id:"mySwiper",autoplay:{delay:2500,disableOnInteraction:!1},children:[e(s,{children:e("img",{src:K})}),e(s,{children:e("img",{src:J})}),e(s,{children:e("img",{src:Q})})]})})}const Z=()=>i("div",{children:[e(X,{}),e(E,{})]});const ee=()=>{const[n,h]=z("mbjwagor"),[a,u]=w.useState(!1);return e(c,{children:i("div",{style:{marginTop:"20px",textAlign:"center",height:"800px"},children:[e("br",{}),e("br",{}),e("b",{style:{color:"#3f2c74",fontSize:"55px"},children:"\xA1PIDELO YA!"})," ",e("br",{}),e("br",{}),e("b",{style:{color:"#3f2c74",fontSize:"18px"},children:"Completa el siguiente formulario y nos comunicaremos"})," ",e("br",{}),e("b",{style:{color:"#3f2c74",fontSize:"18px"},children:"contigo de inmediato"}),e("br",{}),e("br",{}),e("br",{}),e(f,{centered:!0,columns:2,children:i(o,{id:"formPadre",style:{width:"50%"},onSubmit:h,children:[i(f.Row,{centered:!0,columns:4,children:[e(f.Column,{children:i(o.Group,{id:"primeraFila",widths:"equal",children:[i(o.Field,{id:"namesPadre",children:[e("label",{id:"names",style:{color:"#3f2c74"},children:"NOMBRES Y APELLIDOS"}),e(p,{id:"namesx",type:"text",name:"names"}),e(m,{prefix:"Names",field:"names",errors:n.errors})]}),i(o.Field,{children:[e("label",{style:{color:"#3f2c74"},children:"EMAIL"}),e(p,{id:"email",type:"email",name:"email"}),e(m,{prefix:"Email",field:"email",errors:n.errors})]})]})}),e(f.Column,{children:i(o.Group,{widths:"equal",children:[i(o.Field,{children:[e("label",{style:{color:"#3f2c74"},children:"PLAN"}),e(p,{id:"plan",type:"text",name:"plan"}),e(m,{prefix:"Plan",field:"plan",errors:n.errors})]}),i(o.Field,{children:[e("label",{style:{color:"#3f2c74"},children:"VIVIENDA"}),e(p,{id:"vivienda",type:"text",name:"vivienda"}),e(m,{prefix:"Vivienda",field:"vivienda",errors:n.errors})]})]})}),e(f.Column,{children:i(o.Group,{widths:"equal",children:[i(o.Field,{children:[e("label",{style:{color:"#3f2c74"},children:"DISTRITO"}),e(p,{id:"distrito",type:"text",name:"distrito"}),e(m,{prefix:"Distrito",field:"distrito",errors:n.errors})]}),i(o.Field,{children:[e("label",{style:{color:"#3f2c74"},children:"CELULAR"}),e(p,{id:"celular",type:"text",name:"celular"}),e(m,{prefix:"Celular",field:"celular",errors:n.errors})]})]})}),e(f.Column,{children:e(o.Group,{widths:"equal",children:i(o.Field,{children:[e("label",{style:{color:"#3f2c74"},children:"DIRECCI\xD3N"}),e(p,{id:"direccion",type:"text",name:"direccion"}),e(m,{prefix:"Direccion",field:"direccion",errors:n.errors})]})})})]}),i(S,{basic:!0,onClose:()=>u(!1),onOpen:()=>u(!0),open:a,size:"small",trigger:e(l,{id:"botonForm",style:{margin:"auto",fontSize:"25px",fontWeight:"bold",backgroundColor:"#7cbdd2",color:"white",borderRadius:"20px"},disabled:n.submitting,children:"ENVIAR"}),children:[i(P,{icon:!0,children:[e(r,{name:"send"}),e("h1",{children:"Formulario Enviado"})]}),e(S.Content,{children:e("h3",{style:{textAlign:"center"},children:"Nos pondremos en contacto contigo."})}),e(S.Actions,{style:{textAlign:"center"},children:i(l,{color:"green",inverted:!0,onClick:()=>u(!1),children:[e(r,{name:"checkmark"})," OK"]})})]})]})})]})})};var ie="/assets/WallpNos.acc2f9d1.jpg";function re(){return e(c,{children:e("img",{style:{width:"100%"},src:ie})})}const ne=()=>e("div",{children:e(re,{})});var le="/assets/WallpCont.b825a612.png";function te(){return e(c,{children:e("img",{style:{width:"100%"},src:le})})}const oe=()=>e(c,{children:e(te,{})}),de=()=>e(c,{children:e(ee,{})});var ce="/assets/WallpPlanes.c7b3cfca.png";function ae(){return e(c,{children:e("img",{style:{width:"100%"},src:ce})})}const se=()=>i(c,{children:[e(ae,{}),e(E,{})]}),he=()=>e(c,{children:i(D,{children:[e($,{}),i(G,{children:[e(g,{path:"/",element:e(Z,{})}),e(g,{path:"/Nosotros",element:e(ne,{})}),e(g,{path:"/Contacto",element:e(oe,{})}),e(g,{path:"/Planes",element:e(se,{})}),e(g,{path:"/Solicitar",element:e(de,{})})]})]})});W.render(e(w.StrictMode,{children:e(he,{})}),document.getElementById("root"));
